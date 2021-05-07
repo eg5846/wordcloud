bca49b09f37b3f421287111d39e427edd45f8234
Andreas Egner <andreas.egner@web.de>
2021-05-07 08:57:15 +0200

    Unzip services.zip as intial application project

---
+++ src/test/java/org/example/services/WordcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
