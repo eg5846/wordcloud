4261997b9e3e7ea9d30f82d100a707147ed0f54f
Andreas Egner <andreas.egner@web.de>
2021-05-09 19:35:37 +0200

    Reformat generated java source code

--- src/main/java/org/example/services/wordcloud/WordcloudApplication.java
+++ src/main/java/org/example/services/wordcloud/WordcloudApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WordcloudApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WordcloudApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WordcloudApplication.class, args);
+    }
 
 }
